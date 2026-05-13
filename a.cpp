2c781b515643e6d12b54d6a52414cae04eb03931
ryotaro <ryotarobro@gmail.com>
2026-05-13 15:42:12 +0900

    update

--- a.cpp
+++ a.cpp
@@ -1,43 +1,32 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-const long long MOD = 1000000007;
-
-long long modpow(long long a, long long n) {
-    long long r = 1;
-
-    while (n > 0) {
-        if (n & 1) r = r * a % MOD;
-        a = a * a % MOD;
-        n >>= 1;
-    }
-
-    return r;
+long n;
+long cnt(long l){
+	vector<long> v(n);
+	for(int i = 0; i < n; i++) cin>>v[i];
+	sort(v.rbegin(), v.rend());
+	long sum = 0;
+	long r = n;
+	for(int i = 0; i < n; i++){
+		sum += v[i];
+		if(sum > l){
+			r = i + 1;
+			break;
+		}
+	}
+	return r;
 }
 
 int main() {
-    int X, Y;
-    cin >> X >> Y;
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
 
-    int N = X + Y;
-
-    vector<long long> fact(N + 1), invfact(N + 1);
-
-    fact[0] = 1;
-
-    for (int i = 1; i <= N; i++) {
-        fact[i] = fact[i - 1] * i % MOD;
-    }
-
-    invfact[N] = modpow(fact[N], MOD - 2);
-
-    for (int i = N; i >= 1; i--) {
-        invfact[i - 1] = invfact[i] * i % MOD;
-    }
-
-    long long ans =
-        fact[N] * invfact[X] % MOD * invfact[Y] % MOD;
-
-    cout << ans << endl;
+	long x,y;cin>>n>>x>>y;
+	//long a = cnt(x), b = cnt(y);
+	//cout << min(a, b) << endl;
+	//cout << a << ' ' << b << endl;
+	cout << min(cnt(x), cnt(y)) << endl;
 }
---
+++ sample_program/sample_Combination.cpp
@@ -0,0 +1,44 @@
+//演習問題集 051-Combination Hard
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const long long MOD = 1000000007;
+
+long long modpow(long long a, long long n) {
+    long long r = 1;
+
+    while (n > 0) {
+        if (n & 1) r = r * a % MOD;
+        a = a * a % MOD;
+        n >>= 1;
+    }
+
+    return r;
+}
+
+int main() {
+    int X, Y;
+    cin >> X >> Y;
+
+    int N = X + Y;
+
+    vector<long long> fact(N + 1), invfact(N + 1);
+
+    fact[0] = 1;
+
+    for (int i = 1; i <= N; i++) {
+        fact[i] = fact[i - 1] * i % MOD;
+    }
+
+    invfact[N] = modpow(fact[N], MOD - 2);
+
+    for (int i = N; i >= 1; i--) {
+        invfact[i - 1] = invfact[i] * i % MOD;
+    }
+
+    long long ans =
+        fact[N] * invfact[X] % MOD * invfact[Y] % MOD;
+
+    cout << ans << endl;
+}
